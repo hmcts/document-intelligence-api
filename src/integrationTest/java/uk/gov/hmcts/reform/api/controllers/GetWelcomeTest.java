a6e7f7904b50e0865ce56cabfc34da06c8651d3e
josh-uk <joshua.blackmoor@justice.gov.uk>
2025-12-02 14:12:43 +0000

    Update int test

--- src/integrationTest/java/uk/gov/hmcts/reform/api/controllers/GetWelcomeTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/api/controllers/GetWelcomeTest.java
@@ -2,26 +2,44 @@ package uk.gov.hmcts.reform.api.controllers;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import uk.gov.hmcts.reform.api.Application;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@WebMvcTest
+@SpringBootTest(
+    classes = Application.class,
+    webEnvironment = RANDOM_PORT,
+    properties = {
+        "azure.document-intelligence.endpoint=http://localhost",
+        "azure.document-intelligence.key=dummy"
+    }
+)
 class GetWelcomeTest {
 
-    @Autowired
-    private transient MockMvc mockMvc;
+    @LocalServerPort
+    private int port;
 
     @DisplayName("Should welcome upon root request with 200 response code")
     @Test
-    void welcomeRootEndpoint() throws Exception {
-        MvcResult response = mockMvc.perform(get("/")).andExpect(status().isOk()).andReturn();
+    void welcomeRootEndpoint() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:" + port + "/"))
+            .GET()
+            .build();
 
-        assertThat(response.getResponse().getContentAsString()).startsWith("Welcome");
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assertThat(response.statusCode()).isEqualTo(200);
+        assertThat(response.body()).startsWith("Welcome");
     }
 }
